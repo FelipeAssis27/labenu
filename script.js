28154ae5f15fe51c2ab7659603ca942ed184481d
Felipe de Assis <assisfelipe.gomes@gmail.com>
2020-12-25 18:06:44 -0300

    2ª atualização

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ inputEmail.addEventListener('change', function(event) {
 	}
 
 }); 
-
 document.getElementById('validacao').addEventListener('submit', function(){
 	var emailDigitado = document.getElementById("email").value;
-	alert("O e-mail "+emailDigitado+" foi enviado!");	
-	document.getElementById("email").value = "";	
-
+	alert("O e-mail "+emailDigitado+" foi enviado com sucesso!");	
+	document.getElementById("email").value = "";
+	document.getElementById("mensagem").style.visibility = "visible";
+	var div = document.getElementById('usuario');
+	div.innerHTML += "Bem-vindo " + emailDigitado +".";
 }); 
